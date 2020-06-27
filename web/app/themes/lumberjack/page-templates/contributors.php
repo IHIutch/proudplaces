c3b5a577fccf8398fceb91f37ea6a0a58bc8c55a
IHIutch <jbhutch01@gmail.com>
2020-06-27 11:56:32 -0400

    Contributors page template

--- web/app/themes/lumberjack/page-templates/contributors.php
+++ web/app/themes/lumberjack/page-templates/contributors.php
@@ -11,15 +11,20 @@ use Rareloop\Lumberjack\Http\Responses\TimberResponse;
 use App\PostTypes\Contributor;
 use Timber\Timber;
 
+
 class ContributorsController extends Controller
 {
     public function handle()
     {
+
         $context = Timber::get_context();
         $contributors = Contributor::builder()->get();
 
-        $context['contributors'] = $contributors;
+        foreach ($contributors as $c) {
+            $c->image = get_field('image', $c->id);
+        }
 
+        $context['contributors'] = $contributors;
 
         return new TimberResponse('templates/contributors.twig', $context);
     }
